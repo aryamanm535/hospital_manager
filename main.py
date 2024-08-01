8572f5cbc08e54d5bbd86196d31aff742f16c454
Aryaman Modi <69678251+aryamanm535@users.noreply.github.com>
2024-08-01 10:34:15 +0530

    Update main.py
    Added middleware for connection to React frontend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from typing import List
 from models import Base
@@ -14,6 +15,19 @@ from datetime import timedelta
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # React app URL
+    "http://127.0.0.1:3000"   # React app URL
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/users/", response_model=User)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
@@ -35,7 +49,9 @@ def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2Passw
     access_token = auth.create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": access_token, "token_type": "bearer", 
+    "username": user.username,
+    "role": user.role}
 
 @app.post("/doctors/", response_model=Doctor)
 def create_doctor(doctor: DoctorCreate, db: Session = Depends(get_db), current_user: User = Depends(auth.get_current_user)):
@@ -82,3 +98,15 @@ def delete_appointment(appointment_id: int, db: Session = Depends(get_db), curre
 @app.get("/patients/{patient_id}/appointments/", response_model=List[Appointment])
 def get_appointments_by_patient(patient_id: int, db: Session = Depends(get_db), current_user: User = Depends(auth.get_current_user)):
     return crud.get_appointments_by_patient(db, patient_id)
+
+@app.get("/doctors/", response_model=List[Doctor])
+def get_doctors(db: Session = Depends(get_db), current_user: User = Depends(auth.get_current_user)):
+    return crud.get_doctors(db=db)
+
+@app.get("/patients/", response_model=List[Patient])
+def get_patients(db: Session = Depends(get_db), current_user: User = Depends(auth.get_current_user)):
+    return crud.get_patients(db=db)
+
+@app.get("/appointments/", response_model=List[Appointment])
+def get_appointments(db: Session = Depends(get_db), current_user: User = Depends(auth.get_current_user)):
+    return crud.get_appointments(db=db)
